ede7d400ae7ea55b1c36f79d17597cfa179f5540
Shalahur <srpolash1991@gmail.com>
2025-07-12 00:32:25 +0600

    cd

--- src/main/java/com/dev/ops/task2/task2/WebConfig.java
+++ src/main/java/com/dev/ops/task2/task2/WebConfig.java
@@ -5,13 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ForwardedHeaderFilter;
 
-@Configuration
+//@Configuration
 public class WebConfig {
-	@Bean
-	public FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter() {
-		FilterRegistrationBean<ForwardedHeaderFilter> filterRegBean = new FilterRegistrationBean<>();
-		filterRegBean.setFilter(new ForwardedHeaderFilter());
-		filterRegBean.setOrder(0);
-		return filterRegBean;
-	}
+//	@Bean
+//	public FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter() {
+//		FilterRegistrationBean<ForwardedHeaderFilter> filterRegBean = new FilterRegistrationBean<>();
+//		filterRegBean.setFilter(new ForwardedHeaderFilter());
+//		filterRegBean.setOrder(0);
+//		return filterRegBean;
+//	}
 }
