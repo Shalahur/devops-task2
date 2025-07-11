c6a28758a7535e87a2d4becb3bb58384c4ffd831
Shalahur <srpolash1991@gmail.com>
2025-07-11 23:35:22 +0600

    cd

---
+++ src/main/java/com/dev/ops/task2/task2/WebConfig.java
@@ -0,0 +1,17 @@
+package com.dev.ops.task2.task2;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ForwardedHeaderFilter;
+
+@Configuration
+public class WebConfig {
+	@Bean
+	public FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter() {
+		FilterRegistrationBean<ForwardedHeaderFilter> filterRegBean = new FilterRegistrationBean<>();
+		filterRegBean.setFilter(new ForwardedHeaderFilter());
+		filterRegBean.setOrder(0);
+		return filterRegBean;
+	}
+}
